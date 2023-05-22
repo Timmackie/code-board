5a379a659c170e535424f2ef7d8fdca5e568a899
Tim Mackie <timmackie177@gmail.com>
2023-05-22 13:40:42 -0500

    fixed conflicts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,31 +1,7 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
-
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-=======
 const { User, Posts } = require('../models');
 const withAuth = require('../utils/auth');
 
->>>>>>> develop
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
@@ -35,8 +11,6 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-<<<<<<< HEAD
-=======
 
 router.get('/', (req,res) => {
   res.redirect('/5')
@@ -63,5 +37,4 @@ router.get('/:numPosts', withAuth, async (req, res) => {
   }
 });
 
->>>>>>> develop
 module.exports = router;
